563bfd7a2472d753b2bb1dd596c966b4c6e0e8c4
paragnaik19 <paragnaik19@gmail.com>
2020-08-07 19:27:49 +0530

    my first commit

---
+++ src/main/java/com/example/ratelimiter/RateLimiterApplication.java
@@ -0,0 +1,15 @@
+package com.example.ratelimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class RateLimiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateLimiterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ratelimiter/TestController.java
@@ -0,0 +1,21 @@
+package com.example.ratelimiter;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/hello")
+	public ResponseEntity<String> hello() {
+		return ResponseEntity.ok("Hello World");
+	}
+	
+	@GetMapping("/world")
+	public ResponseEntity<String> world() {
+		return ResponseEntity.ok("Hello World");
+	}
+	
+}
